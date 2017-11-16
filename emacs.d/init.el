fc7dc49b54f4c09fff3ef793da5cabc64a37c17e
Niklas Lind <niklas.johan.lind@gmail.com>
2017-11-16 21:03:28 +0100

    .emacs removed smex etc

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -37,31 +37,9 @@
 
 
 
-(add-to-list 'load-path "~/.emacs.d/custom")
-(load "idomenu.el")
-(require 'tree-mode)
-(require 'windata)
-(require 'dirtree)
-;; ido InteractivilyDoThings
-(require 'ido)
-(ido-mode t)
-;; ido Display ido results vertically, rather than horizontally
-;;(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
-(defun ido-disable-line-truncation () (set (make-local-variable 'truncate-lines) nil))
-(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
-(defun ido-define-keys () ;; C-n/p is more intuitive in vertical layout
-  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
-  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
-(add-hook 'ido-setup-hook 'ido-define-keys)
-;; smex, fetched via packages-list-packages
-(smex-initialize) ; Can be omitted. This might cause a (minimal) delay
-                  ; when Smex is auto-initialized on its first run.
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-;; This is your old M-x.
-(global-set-key (kbd "C-M-x") 'execute-extended-command)
 ;; Use M-c for copy since M-w makes me close other applications :(
 (global-set-key "\M-c" 'clipboard-kill-ring-save)
+
 ;; Allow ALT for |[]{}
 (if (boundp 'ns-command-modifier)
     (setq ns-command-modifier 'meta))
